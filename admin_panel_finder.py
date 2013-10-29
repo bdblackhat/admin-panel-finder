3fb4042e0463c66d1c4070d3fd7d45ffbc3d75ec
bdblackhat <jally.salt@gmail.com>
2013-10-30 02:02:32 +0600

    Create admin_panel_finder.py

---
+++ admin_panel_finder.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("link.txt","r");
+	link = raw_input("Enter Site Name \n(ex : example.com or www.example.com ): ")
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print req_link
+
+def Credit():
+	Space(9); print "#####################################"
+	Space(9); print "#   +++ Admin Panel Finder v1 +++   #"
+	Space(9); print "#     Script by Illûmïnåté Ðëmøñ    #"
+	Space(9); print "#    Bangladesh Black Hat Hackers   #"
+	Space(9); print "#####################################"
+
+Credit()
+findAdmin()
